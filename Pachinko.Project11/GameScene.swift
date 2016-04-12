fd3c3fe1adc15124278d04d69496182aebe993cb
ElijahButers <Elijahbuters@gmail.com>
2016-04-12 16:54:31 +0300

    Added makeSlotAt method that  fills the gaps between bouncers with two types of target slots: good ones (colored green) and bad ones (colored red). Added four of them to didMoveToView.

--- Pachinko.Project11/GameScene.swift
+++ Pachinko.Project11/GameScene.swift
@@ -20,6 +20,11 @@ class GameScene: SKScene {
         
         physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
         
+        makeSlotAt(CGPoint(x: 128, y: 0), isGood: true)
+        makeSlotAt(CGPoint(x: 384, y: 0), isGood: true)
+        makeSlotAt(CGPoint(x: 640, y: 0), isGood: true)
+        makeSlotAt(CGPoint(x: 896, y: 0), isGood: true)
+        
         makeBouncerAt(CGPoint(x: 0, y: 0))
         makeBouncerAt(CGPoint(x: 256, y: 0))
         makeBouncerAt(CGPoint(x: 512, y: 0))
@@ -51,5 +56,19 @@ class GameScene: SKScene {
         bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
         bouncer.physicsBody?.dynamic = false
         addChild(bouncer)
+    }
+    
+    func makeSlotAt(position: CGPoint, isGood: Bool) {
+        
+        var slotBase: SKSpriteNode
+        
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+        }
+        
+        slotBase.position = position
+        addChild(slotBase)
     }
 }
